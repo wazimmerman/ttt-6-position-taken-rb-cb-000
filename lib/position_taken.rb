ca5f255390d98964d728c3d9f0cf160fec71bed4
William Zimmerman <william.a.zimmerman@gmail.com>
2019-04-28 02:56:04 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,9 @@
-# code your #position_taken? method here!
+board = [" "," "," "," "," "," "," "," "," "]
+
+def position_taken?(board, index)
+  if board[index] == "" || " "
+    return false
+  elsif board[index] == "X" || "Y"
+    return true
+  end
+end
