72b4f7982d4e3696d82eb436c19176238ecfd7b2
William Zimmerman <william.a.zimmerman@gmail.com>
2019-04-28 02:56:20 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,7 +3,7 @@ board = [" "," "," "," "," "," "," "," "," "]
 def position_taken?(board, index)
   if board[index] == "" || " "
     return false
-  elsif board[index] == "X" || "Y"
+  elsif board[index] == "X" || "O"
     return true
   end
 end
